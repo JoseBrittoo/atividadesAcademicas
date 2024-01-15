42ef76fa5507d399c28e62284000a00a6d672198
Diana de Oliveira Martins <99148060+ddianaom@users.noreply.github.com>
2024-01-14 21:09:28 -0400

    Teste

--- setup/setup/wsgi.py
+++ setup/setup/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projeto_atividade_academica project.
+WSGI config for setup project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_atividade_academica.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
--- usuarios/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    email = models.EmailField(max_length = 254)
-    senha = models.CharField(max_length = 64)
-
-    def __str__(self) -> str:
-        return self.email
