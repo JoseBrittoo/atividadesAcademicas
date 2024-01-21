3ea5f0a7943542fad610d642a738f8ad39b0a7b9
Diana de Oliveira Martins <99148060+ddianaom@users.noreply.github.com>
2024-01-21 11:21:58 -0400

    Tela de Disciplinas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,12 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('cadastro/', views.cadastro, name = 'cadastro'),
     path('valida_cadastro/', views.valida_cadastro, name = 'valida_cadastro'),
-    path('valida_login/', views.valida_login, name = 'valida_login')
+    path('valida_login/', views.valida_login, name = 'valida_login'),
+    path('disciplina/', views.disciplina, name = 'disciplina'),
+    path('caddisciplina/', views.caddisciplina, name = 'caddisciplina'),
+    path('ac/', views.ac, name = 'ac'),
+    path('al/', views.al, name = 'al'),
+    path('md/', views.md, name = 'md'),
+    path('mc/', views.mc, name = 'mc'),
+
 ]
